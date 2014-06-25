a28e980b7f07c0018e65a80eee135e2a0ecd8e3f
David Estes <davydotcom@gmail.com>
2014-06-25 11:55:33 -0400

    need hibernates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,6 +40,10 @@ grails.project.dependency.resolution = {
 
         runtime ":spud-core:0.5.0"
         runtime ":feeds:1.6"
+
+        runtime(':hibernate:3.6.10.13') {
+            export = false
+        }
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
             export = false
